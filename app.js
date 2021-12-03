e9d5eb000579f0188fbb1768953aeb26c9cbc558
Donald Biggers <DuG@tests-Air.attlocal.net>
2021-12-03 10:53:32 -0600

    updating template function

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const inquirer = require("inquirer");
 
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -131,11 +134,15 @@ if (!portfolioData.projects) {
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-  console.log(portfolioData);
-});
-// const fs = require('fs');
+    const pageHTML = generatePage(portfolioData);
 
-// const generatePage = require('./src/page-template.js');
+    // fs.writeFile('./index.html', pageHTML, err => {
+    //   if (err) throw new Error(err);
+
+    //   console.log('Page created! Check out index.html in this directory to see it!');
+    // });
+});
+
 
 // const pageHTML = generatePage(name, github);
 
